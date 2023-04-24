5ed38686dce278d44576026ff004a15f2f78db03
sergiolfaria <srgion@yahoo.com.br>
2023-04-23 21:16:33 -0300

    intime removidos

--- Jogador.java
+++ Jogador.java
@@ -3,16 +3,19 @@ public class Jogador {
     private String nome;
     private String role;
     private int pontuacaoHabilidade;
+
     
 
-    public Jogador(int id, String nome, String role, int pontuacaoHabilidade) {
+    public Jogador(int id, String nome, String role, int pontuacaoHabilidade ) {
         this.id = id;
         this.nome = nome;
         this.role = role;
         this.pontuacaoHabilidade = pontuacaoHabilidade;
+   
+       
        
     }
-
+    
     public int getId() {
         return id;
     }
